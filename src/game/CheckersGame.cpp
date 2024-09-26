c98f698308e8aa7f9482209ba5dd45e234d2a22e
Lindsey <18leaf@gmail.com>
2024-09-26 16:15:03 -0400

    Checkers Game class created and left room for implementations/todos

--- src/game/CheckersGame.cpp
+++ src/game/CheckersGame.cpp
@@ -3,3 +3,94 @@
 //
 
 #include "CheckersGame.h"
+
+#include <iostream>
+
+
+using namespace Game;
+
+CheckersGame::CheckersGame(const std::string &player1Name, std::string &player2Name) {
+	player1 = std::make_unique<Player>(player1Name, true);
+	player2 = std::make_unique<Player>(player2Name, false);
+	currentPlayer = player1.get();
+	board.initializeBoard();
+}
+
+
+void CheckersGame::startGame() {
+	// TODO implement Move Stack to track moves previously made
+	// game loop
+	while (!isGameOver()) {
+		displayGameStatus();
+
+		// try to make new move, check validatiy, process, switch player, else reprompt
+		if (Move move = getPlayerMove(); validateMove(move)) {
+			processMove(move);
+			switchPlayer();
+		} else {
+			// TODO THROW EXCEPTION AND HANDLE
+			std::cout << "Invalid Move. Try Again" << std::endl;
+		}
+	}
+
+	// TODO implement end of Game prompt
+	std::cout << "Game Over" << std::endl;
+	if (currentPlayer == player1.get()) {
+		std::cout << player2->getName() << " wins!\n";
+	} else {
+		std::cout << player1->getName() << " wins!\n";
+	}
+}
+
+void CheckersGame::switchPlayer() {
+	if (currentPlayer == player1.get()) {
+		currentPlayer = player2.get();
+	} else {
+		currentPlayer = player1.get();
+	}
+}
+
+bool CheckersGame::processMove(const Move& move) {
+	// apply move to the board
+	const int startPosition = move.getStartPosition();
+	const int endPosition = move.getEndPosition();
+	const bool isPlayerOne = currentPlayer->isPlayerOne();
+	board.movePiece(startPosition, endPosition, isPlayerOne);
+
+	// handle captures
+	if (move.getIsCapture()) {
+		const int capturedPosition = (startPosition + endPosition) / 2;
+		board.capturePiece(capturedPosition, isPlayerOne);
+	}
+
+	// check king promotion
+	board.promoteToKing(endPosition);
+
+	return true;
+}
+
+bool CheckersGame::isGameOver() {
+	// todo implement logic
+	return false;
+}
+
+void CheckersGame::displayGameStatus() {
+	std::cout << "\nCurrent Board:\n";
+	board.displayBoard();
+	std::cout << currentPlayer->getName() << "'s turn.\n";
+}
+
+Move CheckersGame::getPlayerMove() {
+	int start, end;
+	std::cout << "Enter your move (start end): ";
+	std::cin >> start >> end;
+
+	// TODO CHECK IF CAPTURED
+
+	return {start, end};
+}
+
+bool CheckersGame::validateMove(const Move &move) {
+	bool isPlayerOne = currentPlayer->isPlayerOne();
+	return board.isLegalMove(move.getStartPosition(), move.getEndPosition(), isPlayerOne);
+}
--- src/game/CheckersGame.h
+++ src/game/CheckersGame.h
@@ -4,13 +4,42 @@
 
 #ifndef CHECKERSGAME_H
 #define CHECKERSGAME_H
+#include <memory>
+#include <string>
 
+#include "Board.h"
+#include "Player.h"
+#include "Move.h"
 
+namespace Game
+{
 
 class CheckersGame {
+public:
+	CheckersGame(const std::string& player1Name, std::string& player2Name);
 
+	// initialize loop
+	void startGame();
+
+private:
+	Board board;
+	// unique players initialization
+	std::unique_ptr<Player> player1;
+	std::unique_ptr<Player> player2;
+	// contents of player... either player 1 or 2
+	Player* currentPlayer;
+
+	// game state methods
+	void switchPlayer();
+	bool processMove(const Move& move);
+	bool isGameOver();
+	void displayGameStatus();
+
+	// helpers
+	Move getPlayerMove();
+	bool validateMove(const Move& move);
 };
 
-
+}
 
 #endif //CHECKERSGAME_H
